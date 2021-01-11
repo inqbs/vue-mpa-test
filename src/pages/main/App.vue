f04e294c5f5052929c4e00ec8c6a87591d5ffd6a
inqbs <incubass@outlook.com>
2021-01-11 17:21:18 +0900

    add validation test with veeValidate v3

--- src/pages/main/App.vue
+++ src/pages/main/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <a href="/shop">shop</a>
-    </div>
-    this is main page
+    <b-nav tabs id="nav">
+      <b-nav-item href="/shop">
+        shop
+      </b-nav-item>
+    </b-nav>
+    <b-jumbotron fluid header="Main Page" lead="This is main page" bg-variant="dark" text-variant="light">
+    </b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col col="12">
+          <validation-observer ref="observer" v-slot="{ handleSubmit }">
+            <b-form @submit.stop.prevent="handleSubmit(submit)">
+              <validation-provider
+                name="이메일"
+                :rules="{required: true, email: true}"
+                v-slot="validationContext"
+              >
+                <b-form-group
+                  label="이메일"
+                  label-for="bf-email"
+                >
+                  <b-form-input
+                    id="bf-email"
+                    type="email"
+                    name="email"
+                    :state="getValidationState(validationContext)"
+                    aria-describedby="bf-email-live-feedback"
+                    v-model="value"
+                  ></b-form-input>
+                  <b-form-invalid-feedback id="bf-email-live-feedback">
+                    {{ validationContext.errors[0] }}
+                  </b-form-invalid-feedback>
+                </b-form-group>
+              </validation-provider>
+              <b-button type="submit" variant="primary">확인</b-button>
+            </b-form>
+          </validation-observer>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      value: ''
+    }
+  },
+  methods:{
+    getValidationState({ dirty, validated, valid = null }) {
+      return dirty || validated ? valid : null;
+    },
+    submit(){
+      alert('submit is complete')
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-#nav {
-  padding: 30px;
+a {
+  font-weight: bold;
+  color: #2c3e50;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  &.router-link-exact-active {
+    color: #42b983;
   }
 }
 </style>
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,6 +2,27 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from '@/store';
 
+//  vue-bootstrap (only modal / sidebar / etc..)
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+import { ValidationObserver, ValidationProvider, extend, localize } from 'vee-validate';
+import * as rules from "vee-validate/dist/rules";
+import ko from 'vee-validate/dist/locale/ko.json'
+
+// Install VeeValidate rules and localization
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule]);
+});
+
+localize('ko', ko)
+
+Vue.component("ValidationObserver", ValidationObserver);
+Vue.component("ValidationProvider", ValidationProvider);
+
 new Vue({
   store,
   render: (h) => h(App),
