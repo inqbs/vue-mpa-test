d00bff9e533f8fe1e8a7178b946f1cde972337a7
inqbs <incubass@outlook.com>
2021-01-13 17:25:43 +0900

    add description block in board table

--- src/pages/pagination/components/Board.vue
+++ src/pages/pagination/components/Board.vue
@@ -1,39 +1,56 @@
 <template>
   <b-col sm="12">
-    <b-table 
+    <b-table
       id="board-table"
-      striped 
+      striped
       :items="items"
       :fields="board.fields"
       :per-page="pagination.perPage"
-      :current-page="isServerSidePaging ? 1 :pagination.currentPage"
+      :current-page="isServerSidePaging ? 1 : pagination.currentPage"
       :primary-key="board.idx"
-      :tbody-transition-props="{name:'fade'}"
+      :tbody-transition-props="{ name: 'fade' }"
       small
-    ></b-table>
+    >
+      <template #cell(title)="row">
+        <span @click.stop="row.toggleDetails">
+          {{row.item.title}}
+        </span>
+      </template>
+      <template #row-details="row">
+        <transition name="slide-fade">
+          <b-card>
+            <b-card-title>{{row.item.title}}</b-card-title>
+            <b-card-sub-title class="mb-2 text-right">by {{row.item.author}}, {{row.item.relativeRegdate}}</b-card-sub-title>
+            <hr />
+            <p>
+              {{ row.item.description }}
+            </p>
+          </b-card>
+        </transition>
+      </template>
+    </b-table>
   </b-col>
 </template>
 
 <script>
 export default {
-  name: 'Board',
-  props: ['isServerSidePaging', 'items', 'pagination', 'rows'],
-  computed: {
-  },
-  data(){
+  name: "Board",
+  props: ["isServerSidePaging", "items", "pagination", "rows"],
+  computed: {},
+  data() {
     return {
-      board:{
+      board: {
         fields: [
-          { key: 'idx', label: 'No'},
-          { key: 'title', label: '제목' },
-          { key: 'author', label: '작성자' },
-          { key: 'relativeRegdate', label: '작성일자'},
+          { key: "idx", label: "No" },
+          { key: "title", label: "제목" },
+          { key: "author", label: "작성자" },
+          { key: "relativeRegdate", label: "작성일자" },
         ],
-      }
-    }
+      },
+    };
   },
-  mounted(){
-    console.log('[components/Board] is mounted')
+  mounted() {
+    console.log("[components/Board] is mounted");
   },
 };
 </script>
